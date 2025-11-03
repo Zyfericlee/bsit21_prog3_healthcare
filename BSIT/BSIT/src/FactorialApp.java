1ab723326c5b27e83a354e793ebf88f632d1c3a8
Zyfericlee <masmelazyfril@gmail.com>
2025-11-03 13:29:21 +0800

    This is my first change

---
+++ BSIT/BSIT/src/FactorialApp.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class FactorialApp {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
+
+        int factorial = 1;
+        for (int i = 1; i <= num; i++) {
+            factorial *= i;
+        }
+
+        System.out.println("The factorial of " + num + " is " + factorial);
+    }
+}
